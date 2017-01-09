50d6fde3eaff16576e4512e46e92bafc158b6a8b
antonina.nokhrina <antoninan92@gmail.com>
2017-01-09 14:48:14 +0200

    init commit

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Books catalog</title>
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+	<script src="js/template.js" type="text/javascript"></script>
+</head>
+<body>
+
+
+</body>
+</html>
